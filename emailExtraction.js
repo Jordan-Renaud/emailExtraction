cddb899b95bbca965238d6286fe3193f70815441
Jordan-Renaud <jordan.dj.renaud@gmail.com>
2022-04-27 12:20:31 +0100

    correct number of emails

--- emailExtraction.js
+++ emailExtraction.js
@@ -1,7 +1,12 @@
 const fs = require("fs");
 const file = fs.readFileSync("test.txt", "utf-8");
 
-const wordsInFile = file.toLowerCase().split(" ");
+const linesInFile = file.toLowerCase().split("\r\n");
+const wordsInFile = [];
+linesInFile.forEach((line) => {
+  const words = line.split(" ");
+  words.forEach((word) => wordsInFile.push(word));
+});
 const softwireEmailAddresses = wordsInFile.filter(
   (word) => word.includes("@softwire.com") && !word.endsWith(".comet.net")
 );
