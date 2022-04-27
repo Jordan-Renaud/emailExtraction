d6b41363ec8a34f79f37d3a793f36811a6c7a36a
Jordan-Renaud <jordan.dj.renaud@gmail.com>
2022-04-27 12:34:19 +0100

    add regex matches for @softwire.com

--- emailExtraction.js
+++ emailExtraction.js
@@ -3,14 +3,22 @@ const file = fs.readFileSync("test.txt", "utf-8");
 
 const linesInFile = file.toLowerCase().split("\r\n");
 const wordsInFile = [];
+
 linesInFile.forEach((line) => {
   const words = line.split(" ");
   words.forEach((word) => wordsInFile.push(word));
 });
+
 const softwireEmailAddresses = wordsInFile.filter(
   (word) => word.includes("@softwire.com") && !word.endsWith(".comet.net")
 );
 
 console.log(
-  `Number of softwire email addresses: ${softwireEmailAddresses.length}`
+  `Number of softwire email addresses found without Regex: ${softwireEmailAddresses.length}`
+);
+
+const regexForSoftwireOnly = /@softwire\.com\s/g;
+const foundMatches = file.match(regexForSoftwireOnly);
+console.log(
+  `Number of softwire email addresses found Regex: ${foundMatches.length}`
 );
